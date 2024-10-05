35b0a68c8424bb410db62b0449c63fab8b4afd5a
Serhii Lapin <Serhii_Lapin@epam.com>
2024-10-05 23:05:46 +0200

    update JenkinsfileApiTests, add AfterPushJenkinsfile, ScheduledChromeJenkinsfile

--- src/main/java/com/mynotes/data/JsonSchema.java
+++ src/main/java/com/mynotes/data/JsonSchema.java
@@ -8,5 +8,5 @@ import java.io.File;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class JsonSchema {
 
-    public static final File GET_NOTE_BY_ID_RESPONSE_JSON_SCHEMA = new File("/Users/Serhii_Lapin/Desktop/atmp/demoqa/src/test/resources/testdata/api.jsonschemas/getNoteByIdResponseJsonSchema.json");
+    public static final File GET_NOTE_BY_ID_RESPONSE_JSON_SCHEMA = new File("src/test/resources/testdata/jsonschemas/getNoteByIdResponseJsonSchema.json");
 }
